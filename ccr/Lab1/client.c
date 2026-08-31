f1c6131004981437aefe83f586d3231c6b143a5a
vjrm123 <victor.ramos.mamani@ucsp.edu.pe>
2026-08-31 05:05:00 +0000

    actualizando los bucles

--- ccr/Lab1/client.c
+++ ccr/Lab1/client.c
@@ -1,57 +1,76 @@
- /* Client code in C */
- 
-  #include <sys/types.h>
-  #include <sys/socket.h>
-  #include <netinet/in.h>
-  #include <arpa/inet.h>
-  #include <stdio.h>
-  #include <stdlib.h>
-  #include <string.h>
-  #include <unistd.h>
- 
-  int main(void)
-  {
+/* Client code */
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
     struct sockaddr_in stSockAddr;
     int Res;
     int SocketFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
     int n;
- 
+
+    char buffer[256];
+    string os;
+
     if (-1 == SocketFD)
     {
-      perror("cannot create socket");
-      exit(EXIT_FAILURE);
+        perror("cannot create socket");
+        exit(EXIT_FAILURE);
     }
- 
+
     memset(&stSockAddr, 0, sizeof(struct sockaddr_in));
- 
+
     stSockAddr.sin_family = AF_INET;
     stSockAddr.sin_port = htons(1100);
-    Res = inet_pton(AF_INET, "192.168.1.33", &stSockAddr.sin_addr);
- 
+
+    Res = inet_pton(AF_INET, "127.0.0.1", &stSockAddr.sin_addr);
+
     if (0 > Res)
     {
-      perror("error: first parameter is not a valid address family");
-      close(SocketFD);
-      exit(EXIT_FAILURE);
+        perror("error: first parameter is not a valid address family");
+        close(SocketFD);
+        exit(EXIT_FAILURE);
     }
     else if (0 == Res)
     {
-      perror("char string (second parameter does not contain valid ipaddress");
-      close(SocketFD);
-      exit(EXIT_FAILURE);
+        perror("invalid IP address");
+        close(SocketFD);
+        exit(EXIT_FAILURE);
     }
- 
-    if (-1 == connect(SocketFD, (const struct sockaddr *)&stSockAddr, sizeof(struct sockaddr_in)))
+
+    if (-1 == connect(SocketFD,
+        (const struct sockaddr *)&stSockAddr,
+        sizeof(struct sockaddr_in)))
     {
-      perror("connect failed");
-      close(SocketFD);
-      exit(EXIT_FAILURE);
+        perror("connect failed");
+        close(SocketFD);
+        exit(EXIT_FAILURE);
     }
-    n = write(SocketFD,"Hi, this is Julio.",18);
-    /* perform read write operations ... */
- 
+
+    while (true) {
+
+        cout << "cliente: ";
+        getline(cin,os);
+        n = write(SocketFD, os.c_str(), os.length());
+        bzero(buffer, 256);
+        n = read(SocketFD, buffer, 255);
+        cout << "servidor: " << buffer << endl;
+    
+    }
+
     shutdown(SocketFD, SHUT_RDWR);
- 
     close(SocketFD);
+
     return 0;
-  }
+}
