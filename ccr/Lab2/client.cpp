829f2b35b4051637a59bc61ed29e81526da0265b
vjrm123 <victor.ramos.mamani@ucsp.edu.pe>
2026-09-07 18:51:30 +0000

    tarea de laboratorio 2

--- ccr/Lab2/client.cpp
+++ ccr/Lab2/client.cpp
@@ -1,3 +1,4 @@
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -13,136 +14,143 @@
 
 using namespace std;
 
+#define TAMANIO_ACCION 1
+#define TAMANIO_NICKNAME 7
+#define TAMANIO_MENSAJE 11
+#define TAMANIO_TOTAL (TAMANIO_ACCION + TAMANIO_NICKNAME + TAMANIO_MENSAJE)
+
 atomic<bool> conectado(true);
+string miNickname;
 
-
-void enviarConFormato(int socket, const string& mensaje) {
-    char tipo = 'N';
-    write(socket, &tipo, 1);
-    
-    uint32_t len = htonl(mensaje.length());
-    write(socket, &len, 4);
-    
-    write(socket, mensaje.c_str(), mensaje.length());
+void empaquetarMensaje(char* buffer, char accion, const string& nickname, const string& mensaje) {
+    bzero(buffer, TAMANIO_TOTAL);
+    buffer[0] = accion;
+    strncpy(buffer + 1, nickname.c_str(), TAMANIO_NICKNAME);
+    strncpy(buffer + 1 + TAMANIO_NICKNAME, mensaje.c_str(), TAMANIO_MENSAJE);
 }
 
-string recibirConFormato(int socket) {
-    char buffer[1024];
-    int n;
+void desempaquetarMensaje(const char* buffer, char& accion,string& nickname, string& mensaje) {
+    accion = buffer[0];
+    char nick[TAMANIO_NICKNAME + 1] = {0};
+    strncpy(nick, buffer + 1, TAMANIO_NICKNAME);
+    nickname = string(nick);
+    nickname.erase(nickname.find_last_not_of(' ') + 1);
     
-    bzero(buffer, 1);
-    n = read(socket, buffer, 1);
-    if (n <= 0) return "";
-    char tipo = buffer[0];
-    
-    uint32_t len;
-    n = read(socket, &len, 4);
-    if (n <= 0) return "";
-    len = ntohl(len);
-    
-    if (len > 1023) { 
-        char* mensaje = new char[len + 1];
-        bzero(mensaje, len + 1);
-        n = read(socket, mensaje, len);
-        if (n <= 0) {
-            delete[] mensaje;
-            return "";
-        }
-        mensaje[len] = '\0';
-        string resultado(mensaje);
-        delete[] mensaje;
-        return resultado;
-    } else {
-        bzero(buffer, len + 1);
-        n = read(socket, buffer, len);
-        if (n <= 0) return "";
-        buffer[len] = '\0';
-        return string(buffer);
-    }
+    char msg[TAMANIO_MENSAJE + 1] = {0};
+    strncpy(msg, buffer + 1 + TAMANIO_NICKNAME, TAMANIO_MENSAJE);
+    mensaje = string(msg);
+    mensaje.erase(mensaje.find_last_not_of(' ') + 1);
 }
 
+void enviarMensaje(int socket, char accion, const string& nickname, const string& mensaje) {
+    char buffer[TAMANIO_TOTAL];
+    empaquetarMensaje(buffer, accion, nickname, mensaje);
+    write(socket, buffer, TAMANIO_TOTAL);
+}
+
+bool recibirMensaje(int socket, char& accion,string& nickname, string& mensaje) {
+    char buffer[TAMANIO_TOTAL];
+    bzero(buffer, TAMANIO_TOTAL);
+    int n = read(socket, buffer, TAMANIO_TOTAL);
+    if (n <= 0) return false;
+    desempaquetarMensaje(buffer, accion, nickname, mensaje);
+    return true;
+}
 
 
 void recibirMensajes(int socketFD) {
+    char accion;
+    string nickname, mensaje;
+    
     while(conectado) {
-       
-        string mensaje = recibirConFormato(socketFD);
-
-        if (mensaje.empty()) {
-            cout << "\n[!] desconectado" << endl;
+        if (!recibirMensaje(socketFD, accion, nickname, mensaje)) {
+            cout << "\n[!] Desconectado del servidor" << endl;
             conectado = false;
             break;
         }
-
-        cout << "\r \n" << mensaje << endl;  
-        cout << "cliente: " << flush;
+        
+        if (accion == 'B') {
+            cout << "\r\n" << nickname << ": " << mensaje << endl;
+        } else if (accion == 'M') {
+            cout << "\r\n[" << nickname << "] " << mensaje << endl;
+        } else {
+            cout << "\r\n[Sistema] " << mensaje << endl;
+        }
+        cout << "> " << flush;
     }
 }
 
 void enviarMensajes(int socketFD) {
-    string os;
-
+    string input;
+    
     while(conectado) {
-        cout << "cliente: ";
-        getline(cin, os);
-
-        if(!conectado) break;
-        if(os == "salir" || os == "exit") {
-            enviarConFormato(socketFD, os);  
+        cout << "> ";
+        getline(cin, input);
+        
+        if (!conectado) break;
+        
+        if (input == "/quit" || input == "salir") {
+            enviarMensaje(socketFD, 'Q', miNickname, "Cerrando...");
             conectado = false;
             break;
         }
-        if(!os.empty()) {
-            enviarConFormato(socketFD, os);  
+        
+        if (input.empty()) continue;
+        
+        if (input[0] == '@') {
+            size_t pos = input.find(' ');
+            if (pos != string::npos) {
+                string destino = input.substr(1, pos - 1);
+                string mensaje = input.substr(pos + 1);
+                enviarMensaje(socketFD, 'M', miNickname, 
+                              "@" + destino + " " + mensaje);
+            }
+        } else {
+            
+            enviarMensaje(socketFD, 'M', miNickname, input);
         }
     }
 }
 
 
-int main(void)
-{
+int main(void) {
     struct sockaddr_in stSockAddr;
-    int Res;
     int SocketFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-    int n;
 
-    char buffer[256];
-    string os;
-
-    if (-1 == SocketFD)
-    {
+    if (-1 == SocketFD) {
         perror("cannot create socket");
         exit(EXIT_FAILURE);
     }
 
     memset(&stSockAddr, 0, sizeof(struct sockaddr_in));
-
     stSockAddr.sin_family = AF_INET;
     stSockAddr.sin_port = htons(1100);
 
-    Res = inet_pton(AF_INET, "127.0.0.1", &stSockAddr.sin_addr);
-
-    if (0 > Res)
-    {
-        perror("error: first parameter is not a valid address family");
-        close(SocketFD);
-        exit(EXIT_FAILURE);
-    }
-    else if (0 == Res)
-    {
-        perror("invalid IP address");
+    if (inet_pton(AF_INET, "127.0.0.1", &stSockAddr.sin_addr) <= 0) {
+        perror("invalid address");
         close(SocketFD);
         exit(EXIT_FAILURE);
     }
 
-    if (-1 == connect(SocketFD,(const struct sockaddr *)&stSockAddr,sizeof(struct sockaddr_in)))
-    {
+    if (-1 == connect(SocketFD, (const struct sockaddr *)&stSockAddr, 
+                      sizeof(struct sockaddr_in))) {
         perror("connect failed");
         close(SocketFD);
         exit(EXIT_FAILURE);
     }
 
-    cout << "[!] conectado. escribe 'salir' o 'exit' para desconectarte" << endl;
+    cout << "Ingresa tu nickname (max 7 caracteres): ";
+    getline(cin, miNickname);
+    if (miNickname.length() > TAMANIO_NICKNAME) {
+        miNickname = miNickname.substr(0, TAMANIO_NICKNAME);
+    }
+
+    enviarMensaje(SocketFD, 'N', miNickname, "Conectando...");
+    
+    cout << "[!] Conectado como: " << miNickname << endl;
+    cout << "[!] Comandos: /quit para salir, @nick mensaje para privado" << endl;
+    cout << "=====================================" << endl;
+
     thread hiloRecibir(recibirMensajes, SocketFD);
     thread hiloEnviar(enviarMensajes, SocketFD);
 
--- ccr/Lab2/server.cpp
+++ ccr/Lab2/server.cpp
@@ -11,176 +11,205 @@
 #include <string>
 #include <thread>
 #include <vector>
+#include <map>
 #include <mutex>
 #include <algorithm>
 
 using namespace std;
 
-vector<int> clients;
-mutex mtxClients;
+#define TAMANIO_ACCION 1
+#define TAMANIO_NICKNAME 7
+#define TAMANIO_MENSAJE 11
+#define TAMANIO_TOTAL (TAMANIO_ACCION + TAMANIO_NICKNAME + TAMANIO_MENSAJE)
+
+struct ClienteInfo {
+    int socket;
+    string nickname;
+};
+
+map<string, int> listaDeClientes;
+mutex mtxClientes;
 
 
-void enviarConFormato(int socket, const string& mensaje) {
-    char tipo = 'N';
-    write(socket, &tipo, 1);
+void empaquetarMensaje(char* buffer, char accion, const string& nickname, const string& mensaje) {
+    bzero(buffer, TAMANIO_TOTAL);
     
-    uint32_t len = htonl(mensaje.length());
-    write(socket, &len, 4);
+    buffer[0] = accion;
     
-    write(socket, mensaje.c_str(), mensaje.length());
+    int nickLen = nickname.length();
+    if (nickLen > TAMANIO_NICKNAME) nickLen = TAMANIO_NICKNAME;
+    strncpy(buffer + 1, nickname.c_str(), nickLen);
+    
+    int msgLen = mensaje.length();
+    if (msgLen > TAMANIO_MENSAJE) msgLen = TAMANIO_MENSAJE;
+    strncpy(buffer + 1 + TAMANIO_NICKNAME, mensaje.c_str(), msgLen);
 }
 
-// Recibir mensaje con formato: [TIPO][TAMAÑO 4B][MENSAJE]
-string recibirConFormato(int socket) {
-    char buffer[1024];
-    int n;
-    
-    bzero(buffer, 1);
-    n = read(socket, buffer, 1);
-    if (n <= 0) return "";
-    char tipo = buffer[0];
+void desempaquetarMensaje(const char* buffer, char& accion,string& nickname, string& mensaje) {
 
-    uint32_t len;
-    n = read(socket, &len, 4);
-    if (n <= 0) return "";
-    len = ntohl(len);
+    accion = buffer[0];
     
-    if (len > 1023) {  
+    char nick[TAMANIO_NICKNAME + 1] = {0};
+    strncpy(nick, buffer + 1, TAMANIO_NICKNAME);
+    nickname = string(nick);
 
-        char* mensaje = new char[len + 1];
-        bzero(mensaje, len + 1);
-        n = read(socket, mensaje, len);
-        if (n <= 0) {
-            delete[] mensaje;
-            return "";
+    nickname.erase(nickname.find_last_not_of(' ') + 1);
+    
+    char msg[TAMANIO_MENSAJE + 1] = {0};
+    strncpy(msg, buffer + 1 + TAMANIO_NICKNAME, TAMANIO_MENSAJE);
+    mensaje = string(msg);
+    mensaje.erase(mensaje.find_last_not_of(' ') + 1);
+}
+
+void enviarMensaje(int socket, char accion, const string& nickname, const string& mensaje) {
+    char buffer[TAMANIO_TOTAL];
+    empaquetarMensaje(buffer, accion, nickname, mensaje);
+    write(socket, buffer, TAMANIO_TOTAL);
+}
+
+bool recibirMensaje(int socket, char& accion,string& nickname, string& mensaje) {
+    char buffer[TAMANIO_TOTAL];
+    bzero(buffer, TAMANIO_TOTAL);
+    int n = read(socket, buffer, TAMANIO_TOTAL);
+    if (n <= 0) return false;
+    desempaquetarMensaje(buffer, accion, nickname, mensaje);
+    return true;
+}
+
+
+void broadcastMensaje(char accion, const string& nickname, const string& mensaje, int socketFD) {
+    lock_guard<mutex> lock(mtxClientes);
+    for(const auto& cliente : listaDeClientes) {
+        if(cliente.second != socketFD) {
+            enviarMensaje(cliente.second, accion, nickname, mensaje);
         }
-
-        mensaje[len] = '\0';
-        string resultado(mensaje);
-        delete[] mensaje;
-        return resultado;
-
-    } else {
-
-        bzero(buffer, len + 1);
-        n = read(socket, buffer, len);
-        if (n <= 0) return "";
-        buffer[len] = '\0';
-        return string(buffer);
-
     }
 }
 
-
-void broadcastMensaje(const string& mensaje, int socketFD) {
-    lock_guard<mutex> lock(mtxClients);
-    for(int clientFD : clients) {
-        if(clientFD != socketFD) {
-            enviarConFormato(clientFD, mensaje);  
-        }
+void enviarMensajePrivado(const string& destino, char accion,
+                          const string& nickname, const string& mensaje) {
+    lock_guard<mutex> lock(mtxClientes);
+    auto it = listaDeClientes.find(destino);
+    if (it != listaDeClientes.end()) {
+        enviarMensaje(it->second, accion, nickname, mensaje);
     }
 }
 
-void eliminarCliente(int socketFD) {
-    lock_guard<mutex> lock(mtxClients);
-    clients.erase(remove(clients.begin(), clients.end(), socketFD), clients.end());
+void eliminarCliente(const string& nickname) {
+    lock_guard<mutex> lock(mtxClientes);
+    auto it = listaDeClientes.find(nickname);
+    if (it != listaDeClientes.end()) {
+        close(it->second);
+        listaDeClientes.erase(it);
+    }
 }
 
 void manejarCliente(int socketFD) {
-    char buffer[256];
-    int n;
+    char accion;
+    string nickname, mensaje;
+    
+    if (!recibirMensaje(socketFD, accion, nickname, mensaje)) {
+        close(socketFD);
+        return;
+    }
+
+    if (accion != 'N') {
+        cout << "[-] Error: Se esperaba registro (N)" << endl;
+        close(socketFD);
+        return;
+    }
+    
     {
-        lock_guard<mutex> lock(mtxClients);
-        clients.push_back(socketFD);
-        cout << "[+] Cliente conectado. Total clientes: " << clients.size() << endl;
+        lock_guard<mutex> lock(mtxClientes);
+        listaDeClientes[nickname] = socketFD;
+        cout << "[+] " << nickname << " se ha conectado. Total: " 
+             << listaDeClientes.size() << endl;
     }
-
-    string bienvenida = "bienvenido al chat. Escribe 'salir' o 'exit' para desconectarte.";
-    enviarConFormato(socketFD, bienvenida);  // CAMBIADO
-
+    
+    broadcastMensaje('B', "Sistema", nickname + " se ha unido al chat.", socketFD);
+    
     while(true) {
-        string mensaje = recibirConFormato(socketFD);
-
-        if (mensaje.empty()) {
-            cout << "[-] Cliente desconectado. Total clientes: " << clients.size() << endl;
-            eliminarCliente(socketFD);
+        if (!recibirMensaje(socketFD, accion, nickname, mensaje)) {
             break;
         }
-
-        if(mensaje == "salir" || mensaje == "exit") {
-            string despedida = "desconectado del chat.\n";
-            enviarConFormato(socketFD, despedida);  // CAMBIADO
-            eliminarCliente(socketFD);
-            cout << "[-] Cliente desconectado. Total: " << clients.size() << endl;
-            break;
+        
+        switch(accion) {
+            case 'M':  
+                cout << nickname << ": " << mensaje << endl;
+                broadcastMensaje('M', nickname, mensaje, socketFD);
+                break;
+                
+            case 'Q':  
+                cout << "[-] " << nickname << " se ha desconectado" << endl;
+                eliminarCliente(nickname);
+                broadcastMensaje('B', "Sistema", 
+                                 nickname + " ha salido del chat.", socketFD);
+                close(socketFD);
+                return;
+                
+            case 'B':  
+                cout << "[BROADCAST] " << nickname << ": " << mensaje << endl;
+                broadcastMensaje('B', nickname, mensaje, socketFD);
+                break;
+                
+            default:
+                cout << "[!] Acción desconocida: " << accion << endl;
+                break;
         }
-
-        cout << "cliente: " << mensaje << endl;
-        broadcastMensaje(mensaje, socketFD);
     }
-
-    shutdown(socketFD, SHUT_RDWR);
+    
+    eliminarCliente(nickname);
+    broadcastMensaje('B', "Sistema", nickname + " ha salido del chat.", socketFD);
     close(socketFD);
 }
 
 
-int main(void)
-{
+int main(void) {
     struct sockaddr_in stSockAddr;
     int SocketFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-    char buffer[256];
-    int n;
-
-    string os;
-
-    if(-1 == SocketFD)
-    {
+    
+    if(-1 == SocketFD) {
         perror("can not create socket");
         exit(EXIT_FAILURE);
     }
-
+    
+    int opt = 1;
+    setsockopt(SocketFD, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    
     memset(&stSockAddr, 0, sizeof(struct sockaddr_in));
-
     stSockAddr.sin_family = AF_INET;
     stSockAddr.sin_port = htons(1100);
     stSockAddr.sin_addr.s_addr = INADDR_ANY;
-
-    if(-1 == bind(SocketFD,
-        (const struct sockaddr *)&stSockAddr,
-        sizeof(struct sockaddr_in)))
-    {
+    
+    if(-1 == bind(SocketFD, (const struct sockaddr *)&stSockAddr, 
+                  sizeof(struct sockaddr_in))) {
         perror("error bind failed");
         close(SocketFD);
         exit(EXIT_FAILURE);
     }
-
-    if(-1 == listen(SocketFD, 10))
-    {
+    
+    if(-1 == listen(SocketFD, 10)) {
         perror("error listen failed");
         close(SocketFD);
         exit(EXIT_FAILURE);
     }
-
+    
+    cout << "[+] Servidor iniciado en puerto 1100" << endl;
+    cout << "[+] Protocolo: " << TAMANIO_TOTAL << " bytes por mensaje" << endl;
+    cout << "[+] Esperando clientes..." << endl;
+    
     vector<thread> threads;
-
+    
     for(;;) {
         int ConnectFD = accept(SocketFD, NULL, NULL);
-
         if(0 > ConnectFD) {
             perror("error accept failed");
-            close(SocketFD);
-            exit(EXIT_FAILURE);
+            continue;
         }
-        threads.emplace_back(thread(manejarCliente, ConnectFD));
+        threads.emplace_back(manejarCliente, ConnectFD);
         threads.back().detach();
     }
-
-    for(auto& t : threads) {
-        if(t.joinable()) {
-            t.join();
-        }
-    }
-
+    
     close(SocketFD);
     return 0;
 }
